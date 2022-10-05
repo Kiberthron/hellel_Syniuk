134f14aa448b59d0897ad719fe395e01730fe2c5
maksym <woweurope11@gmail.com>
2022-10-05 15:13:27 +0300

    Created 3 variables with the same data and the same id's Created 2 variables with the same data and the differents id's Changed their typos

---
+++ Lesson_3/home_2.py
@@ -0,0 +1,47 @@
+board_id = "C02T41BPGTFM"
+serial = "C02T41BPGTFM"
+number = "C02T41BPGTFM"
+
+print(board_id == serial == number)
+print(id(board_id))
+print(board_id is serial is number)
+
+def mainFunction():
+    global board_id
+    global serial
+    global number
+    board_id = list(board_id)
+    serial = list(serial)
+    number = list(number)
+    print(board_id == serial == number)
+    print(id(board_id))
+    print(id(serial))
+    print(id(number))
+    print(board_id is serial is number)
+mainFunction()
+
+model = [11.1]
+model_id = [11.1]
+
+print(id(model))
+print(id(model_id))
+print(model == model_id)
+print(model is model_id)
+
+model_id = model
+
+print(model == model_id)
+print(model is model_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
