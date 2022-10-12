e38b09dbc63b7b4055a6bd6af2a88c47e0de76f0
maksimsinuk <woweurope11@gmail.com>
2022-10-12 16:48:10 +0300

    Written a program that will get the name and age of the user The program must be wrapped in an eternal loop

---
+++ lesson_5/home_4.py
@@ -0,0 +1,16 @@
+while True:
+    name = input("Введите имя: ")
+    age = input("Сколько Вам лет: ")
+    if not age.isdigit() or int(age) <= 0:
+        print("Ошибка, повторите ввод")
+    elif int(age) < 10:
+        print("Привет, шкет", name)
+    elif 10 <= int(age) <= 18:
+        print("Как жизнь", name)
+    elif 18 < int(age) < 100:
+        print("Что желаете", name)
+    else:
+        print(name, "вы лжете - в наше время столько не живут", sep=', ')
+    answer = input("Желаете выйти? (Д/Y) ")
+    if answer.upper() in ("Y", "Д"):
+        break
