56d5d2145be5b6cdb36e3b39f458fbd82ddc8180
kiberthron <woweurope11@gmail.com>
2022-10-23 21:23:41 +0300

    Profit according to table invoice_items. Order amount = UnitPrice *Quantity  Counting the number of repetitions of FirstName in the customers table

---
+++ Mentor_1/Home_1.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+
+db_pass = os.path.join(os.getcwd(), 'chinook.db')
+connection = sqlite3.connect(db_pass)
+cur = connection.cursor()
+
+sql_db = '''
+    SELECT SUM (UnitPrice) * SUM (Quantity)
+        FROM invoice_items;
+'''
+result = cur.execute(sql_db).fetchall()
+
+for result in result:
+    print(*result)
+
+sql_db = '''
+    SELECT FirstName
+        FROM customers;
+'''
+result = cur.execute(sql_db).fetchall()
+
+array_d = {}.fromkeys(result, 0)
+for a in result:
+    array_d[a] += 1
+print(array_d)
+
+
+
+
