cf6123c5402342cd764e88fc3df34533ac59231e
kiberthron <woweurope11@gmail.com>
2022-12-02 13:38:47 +0200

    main crate csv

--- Diplom/New_data.py
+++ Diplom/New_data.py
@@ -1,26 +1,21 @@
 import csv
 
 def new_data():
-   while True:
-      en_name = input("Name: ")
-      name = en_name.split()
-      en_surname = input("Surname: ")
-      surname = en_surname.split()
-      en_age = input("Age: ")
-      age = en_age.split()
-      en_birth = input("Brith: ")
-      birth = en_birth.split()
-      en_death = input("Death: ")
-   # if en_death is None:
-   #    en_death = "-"
-      death = en_death.split()
-      list_data = surname + name + age + birth + death
-      with open('db.csv', 'a') as csvfile:
-         writer = csv.writer(csvfile)
-         writer.writerow(list_data)
-      answer = input('stop enter data? ')
-      if answer.upper() in ("Y"):
-         break
+   en_name = input("Name: ")
+   name = en_name.split()
+   en_surname = input("Surname: ")
+   surname = en_surname.split()
+   en_age = input("Age: ")
+   age = en_age.split()
+   en_birth = input("Brith: ")
+   birth = en_birth.split()
+   en_death = input("Death: ")
+
+   death = en_death.split()
+   list_data = surname + name + age + birth + death
+   with open('db.csv', 'a') as csvfile:
+       writer = csv.writer(csvfile)
+       writer.writerow(list_data)
 
 
 
